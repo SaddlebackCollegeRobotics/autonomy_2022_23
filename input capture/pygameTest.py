a0a794fc6044454d5ef89318b8d8059b294f071e
Supernova1114 <supernovawarriors128@gmail.com>
2022-12-24 04:19:17 -0800

    Trying to get multithreading working

--- input capture/pygameTest.py
+++ input capture/pygameTest.py
@@ -5,6 +5,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame
 import fileinput
+import threading
 
 pygame.init()
 gamepadDict = {}
@@ -63,142 +64,92 @@ def onGamepadRemoved(event):
     print(f"Gamepad {event.instance_id} Disconnected")
 
 
-# Handle gamepad onButtonDown events
-def onButtonDown(event):
+# Handle gamepad button events
+def onButtonEvent(event, callbackArray):
 
     # Grab gamepad layout from dictionary
     layout = gamepadDict[event.instance_id][1]
 
-    # North
-    if event.button == layout[0]:
-        print("North")
+    # Call function corresponding to button pressed
+    for i in range(len(layout)):
+        if (event.button == layout[i]):
+            callbackArray[i]()
+            break
 
-    # West
-    if event.button == layout[1]:
-        print("West")
 
-    # South
-    if event.button == layout[2]:
-        print("South")
+# Calls functions corresponding to button DOWN events
+def getButtonDownCallback(callbackArray):
 
-    # East
-    if event.button == layout[3]:
-        print("East")
-    
-    # Share
-    if event.button == layout[4]:
-        print("Share")
-
-    # Options
-    if event.button == layout[5]:
-        print("Options")
-
-    # Home
-    if event.button == layout[6]:
-        print("Home")
-    
-    # L1 (Left Bumper)
-    if event.button == layout[7]:
-        print("L1")
-
-    # R1 (Right Bumper)
-    if event.button == layout[8]:
-        print("R1")
-
-    # L3 (Left Stick)
-    if event.button == layout[9]:
-        print("L3")
-
-    # R3 (Right Stick)
-    if event.button == layout[10]:
-        print("R3")
+    def async_wrapper():
         
+        loop = True
+        while loop:
+            
+            for event in pygame.event.get():
+                # Get button DOWN gamepad events
+                if event.type == pygame.JOYBUTTONDOWN:
+                    onButtonEvent(event, callbackArray)
 
-# Handle gamepad onButtonUp events
-def onButtonUp(event):
-
-    # Grab gamepad layout from dictionary
-    layout = gamepadDict[event.instance_id][1]
-
-    # North
-    if event.button == layout[0]:
-        ...
-
-    # West
-    if event.button == layout[1]:
-        ...
-
-    # South
-    if event.button == layout[2]:
-        ...
-
-    # East
-    if event.button == layout[3]:
-        ...
+    t1 = threading.Thread(target=async_wrapper)
+    t1.run()
     
-    # Share
-    if event.button == layout[4]:
-        ...
 
-    # Options
-    if event.button == layout[5]:
-        ...
-
-    # Home
-    if event.button == layout[6]:
-        ...
-    
-    # L1 (Left Bumper)
-    if event.button == layout[7]:
-        ...
-
-    # R1 (Right Bumper)
-    if event.button == layout[8]:
-        ...
-
-    # L3 (Left Stick)
-    if event.button == layout[9]:
-        ...
-
-    # R3 (Right Stick)
-    if event.button == layout[10]:
-        ...
-
-
-# main loop
-def main():
-
-    print("Input Capture Started")
+# Calls functions corresponding to button UP events
+def getButtonUpCallback(callbackArray):
 
     loop = True
     while loop:
 
-        # Handle events
         for event in pygame.event.get():
 
-            # Get Button DOWN gamepad events
+            # Get button UP gamepad events
             if event.type == pygame.JOYBUTTONDOWN:
-                onButtonDown(event)
+                onButtonEvent(event, callbackArray)
 
-            # Get Button UP gamepad events
-            if event.type == pygame.JOYBUTTONUP:
-                onButtonUp(event)
 
-            # Handle new gamepad connections
-            if event.type == pygame.JOYDEVICEADDED:
-                onGamepadConnected(event)
+# Main event loop
+def init():
 
-            # Handle gamepad disconnections
-            if event.type == pygame.JOYDEVICEREMOVED:
-                onGamepadRemoved(event)
+    def async_wrapper():
+        loop = True
+        while loop:
 
-            # Handle gamepad hat buttons
-            if event.type == pygame.JOYHATMOTION:
-                ...
+            # Handle events
+            for event in pygame.event.get():
 
-        
+                # Get Button DOWN gamepad events
+                if event.type == pygame.JOYBUTTONDOWN:
+                    ...
 
-# Run the program
-if __name__ == "__main__":
-    main()
-    pygame.quit()
+                # Get Button UP gamepad events
+                if event.type == pygame.JOYBUTTONUP:
+                    ...
+
+                # Handle new gamepad connections
+                if event.type == pygame.JOYDEVICEADDED:
+                    onGamepadConnected(event)
+
+                # Handle gamepad disconnections
+                if event.type == pygame.JOYDEVICEREMOVED:
+                    onGamepadRemoved(event)
+
+                # Handle gamepad hat buttons
+                if event.type == pygame.JOYHATMOTION:
+                    ...
+
+        pygame.quit()
+
+    t1 = threading.Thread(target=async_wrapper)
+    t1.run()
+
+
+# # main loop
+# def main():
+#     print("Input Capture Started")
+#     startEventLoop()
+
+
+# # Run the program
+# if __name__ == "__main__":
+#     main()
+#     pygame.quit()
