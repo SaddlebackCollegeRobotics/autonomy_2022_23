6333f434f1646ac55ba0f948fb413ac81cad51b1
jasper <49565505+wluxie@users.noreply.github.com>
2023-02-26 15:35:33 -0800

    visualizer subscribes correct topic, not raw cam

--- aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_visualizer.py
+++ aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_visualizer.py
@@ -12,12 +12,17 @@ class Tag_Visualizer(Node):
     def __init__(self):
         super().__init__('tag_visualizer')
 
-        self.subscription = self.create_subscription(Image, 'autonomy/camera_image', self.listener_callback, 10)
+        self.subscription = self.create_subscription(Image, 'autonomy/tag_visualization', self.listener_callback, 10)
         self.bridge = CvBridge()
+
+        self.get_logger().info('tag_visualizer node started')
+
 
     def listener_callback(self, image_msg):
         frame = self.bridge.imgmsg_to_cv2(image_msg)
         cv2.imshow('tag orientation visualization', frame)
+
+        self.get_logger().info('diaplaying image')
 
 def main(args=None):
     rclpy.init(args=args)
