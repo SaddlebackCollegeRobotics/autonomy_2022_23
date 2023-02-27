50e8adf663a76e054e316dbbe546422bd8a4ee4e
jasper <49565505+wluxie@users.noreply.github.com>
2023-02-26 16:01:20 -0800

    tag visualizer finished

--- aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_visualizer.py
+++ aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_visualizer.py
@@ -1,9 +1,11 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.qos import qos_profile_sensor_data
+
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 
-import numpy as np
 import cv2
 
 
@@ -12,15 +14,20 @@ class Tag_Visualizer(Node):
     def __init__(self):
         super().__init__('tag_visualizer')
 
-        self.subscription = self.create_subscription(Image, 'autonomy/tag_visualization', self.listener_callback, 10)
+        self.subscription = self.create_subscription(CompressedImage, 
+                                                     'autonomy/tag_visualization', 
+                                                     self.listener_callback, 
+                                                     qos_profile=qos_profile_sensor_data, 
+                                                     callback_group=ReentrantCallbackGroup())
         self.bridge = CvBridge()
 
         self.get_logger().info('tag_visualizer node started')
 
 
     def listener_callback(self, image_msg):
-        frame = self.bridge.imgmsg_to_cv2(image_msg)
+        frame = self.bridge.compressed_imgmsg_to_cv2(image_msg)
         cv2.imshow('tag orientation visualization', frame)
+        cv2.waitKey(1)
 
         self.get_logger().info('diaplaying image')
 
