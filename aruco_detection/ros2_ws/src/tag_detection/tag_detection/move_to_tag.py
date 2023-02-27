5ae7527acb7a5c7035ad98ea00b94458e7aef290
Jasper <49565505+wluxie@users.noreply.github.com>
2023-02-27 01:26:11 -0800

    Minor change & fix print_debug

--- aruco_detection/ros2_ws/src/tag_detection/tag_detection/move_to_tag.py
+++ aruco_detection/ros2_ws/src/tag_detection/tag_detection/move_to_tag.py
@@ -7,16 +7,25 @@ from tag_interface.msg import TagList
 
 from time import perf_counter, sleep
 
+
+# HYPERPARAMETERS / CONST
+TARGET_DIST = 400.0             # cm
+DEAD_ZONE = 0.1                 # 0 <= deadzone <= 1
+TIME_TO_MOVE_THROUGH_GOAL = 2   # seconds
+TIME_OUT = 5                    # seconds
+
+
 class TagFollower(Node):
+
+    STOP = [0.0, 0.0]
+    FORWARD = [-1.0, -1.0]
+    SPIN = [-1.0, 1.0]
+    MOVE_LEFT = [-0.1, -1.0]
+    MOVE_RIGHT = [-1.0, -0.1]
+
     def __init__(self):
         super().__init__('tag_follower')
-        # HYPERPARAMETERS
-        self.target_dist = 400.0  # cm
-        self.deadzone = 0.1  # 0 <= deadzone <= 1
-        self.time_to_move_through_goal = 2  # seconds
-        self.timeout = 5  # seconds
         timer_period = 0.1  # seconds
-
 
         # INITIALIATIONS
         self.subscription = self.create_subscription(
@@ -37,6 +46,7 @@ class TagFollower(Node):
         self.moving_through_goal = False
         self.time_goal_move_started = 0
 
+
     def listener_callback(self, tag_msg):  # read tag data
         self.tags_found = []
         
@@ -47,7 +57,8 @@ class TagFollower(Node):
 
     def timer_callback(self):  # write move instrucitons from tag data        
         move_msg = Float64MultiArray()
-        move_msg.data = [0.0, 0.0]
+        move_msg.data = TagFollower.STOP
+        vertices = [move_msg.data[0], move_msg.data[1]]
 
         # If we see any tags, update current distance and time
         if len(self.tags_found) > 0:
@@ -55,39 +66,41 @@ class TagFollower(Node):
             self.time_tag_seen = perf_counter()
 
         # If we've reached the goal, stop 
-        if self.curr_dist <= self.target_dist:
-            move_msg.data = [0.0, 0.0]
+        if self.curr_dist <= TARGET_DIST:
+            move_msg.data = TagFollower.STOP
             self.publisher_.publish(move_msg)
-            print_debug(move_msg.data[0], move_msg.data[1])
+            self.print_debug(vertices)
             return
 
         # If we haven't seen a tag in <timeout> seconds, spin around
         # and look for tag
-        if perf_counter() - self.time_tag_seen > self.timeout:
-            move_msg.data = [-1.0, 1.0]
+        if perf_counter() - self.time_tag_seen > TIME_OUT:
+            move_msg.data = TagFollower.SPIN
             self.publisher_.publish(move_msg)
-            print_debug(move_msg.data[0], move_msg.data[1])
+            self.print_debug(vertices)
             return
 
         if len(self.tags_found) == 0: 
-            print_debug(move_msg.data[0], move_msg.data[1])
+            self.print_debug(vertices)
             return
 
         target_dist_from_center = self.tags_found[0].x_position
 
-        if abs(target_dist_from_center) < self.deadzone:  
-            move_msg.data = [1.0, 1.0]
+        if abs(target_dist_from_center) < DEAD_ZONE:  
+            move_msg.data = TagFollower.FORWARD
         elif target_dist_from_center < 0:
-            move_msg.data = [0.1, 1.0]
+            move_msg.data = TagFollower.MOVE_LEFT
         else:
-            move_msg.data = [1.0, 0.1]
+            move_msg.data = TagFollower.MOVE_RIGHT
 
         self.publisher_.publish(move_msg)
 
-    def print_debug(v1, v2):
+
+    def print_debug(self, v: list):
         self.get_logger().info(
-            f'moving autonomously with velocities ({v1}, {v2})'
+            f'moving autonomously with velocities ({v[0]}, {v[1]})'
         )
+
 
 
 def main(args=None):
