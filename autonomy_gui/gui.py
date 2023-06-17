94e202e6e73da4158d76251015b9401db90dbe19
cam <camwolff02@gmail.com>
2023-06-17 09:36:51 -0700

    cleaned gui file

--- autonomy_gui/gui.py
+++ autonomy_gui/gui.py
@@ -4,32 +4,27 @@ from rclpy.node import Node
 import streamlit as st
 
 from sensor_msgs.msg import NavSatFix
-from std_msgs.msg import Bool
 from autonomy_interface.msg import Mission
 
-import threading
 
 mission_topic_name = '/autonomy/mission'
-result_topic_name = '/autonomy/result'
-mission = Mission()
-result = None
 
-def callback(msg):
-    global result
-    result = msg.data
 
 @st.cache_resource
 def create_publisher():
     rclpy.init(args=None)
-    node = Node('gui_pub')
-    return node.create_publisher(Mission, mission_topic_name, 10)
+    return Node('gui_pub').create_publisher(Mission, mission_topic_name, 10)
+
 
 pub = create_publisher()
+mission = Mission()
+
 
 def run_mission():
     print('running mission')
     mission.manual = False
     pub.publish(mission)
+
 
 def run_manual_override():
     print('overriding')
@@ -42,9 +37,9 @@ with st.container():
     st.write('---')
 
     goal_type = st.radio('Choose goal type', ['GNSS Only', 'Post', 'Gate'])
-            
+
     match goal_type:
-        case 'GNSS Only': 
+        case 'GNSS Only':
             mission.goal_type = Mission.GNSS_ONLY
         case 'Post':
             mission.goal_type = Mission.POST
@@ -57,15 +52,15 @@ with st.container():
 
     for n in range(num_coords):
         coord = NavSatFix()
-        coord.latitude = left_col.number_input('latitude', min_value=-90.0, max_value=90.0, key=f'lat{n}')
-        coord.longitude = right_col.number_input('longitude', min_value=-180.0, max_value=180.0, key=f'lon{n}')
+        coord.latitude = left_col.number_input(
+            'latitude', min_value=-90.0, max_value=90.0, key=f'lat{n}')
+        coord.longitude = right_col.number_input(
+            'longitude', min_value=-180.0, max_value=180.0, key=f'lon{n}')
         mission.coordinates.append(coord)
 
     st.write('---')
 
     left_col, right_col = st.columns(2)
     start_btn = left_col.button('Start Mission', on_click=run_mission)
-    manual_btn = right_col.button('Manual Override', on_click=run_manual_override)
-
-    if result is not None:
-        st.write(result)
+    manual_btn = right_col.button(
+        'Manual Override', on_click=run_manual_override)
